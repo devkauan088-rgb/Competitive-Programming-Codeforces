bfe8c68d866af5fc508e2e16bf883aa5791b4403
Kauan <319667624+devkauan088-rgb@users.noreply.github.com>
2026-09-21 17:19:01 -0300

    Solved problem 4A Watermelon

---
+++ Rating-800/Watermelon-4A/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number; cin >> number;
+    if (number % 2 == 0 && number > 2){
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
